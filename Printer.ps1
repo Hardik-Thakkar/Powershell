4b8df5dcf2ba5e4619a8d72f88d85572291a89c0
Hardik-Thakkar <117873560+Hardik-Thakkar@users.noreply.github.com>
2024-05-27 03:34:11 -0700

    Update Printer.ps1

--- Printer.ps1
+++ Printer.ps1
@@ -1,4 +1,4 @@
-Add-Printer -ConnectionName "\\gmepe-s4\HB BIG TOSHIBA"
-Add-Printer -ConnectionName "\\gmepe-dc1\20-HB-TOSHIBA"
+Add-Printer -ConnectionName "\\s4\HB BIG TOSHIBA"
+Add-Printer -ConnectionName "\\dc1\20-HB-TOSHIBA"
 $printer = Get-CimInstance -Class Win32_Printer -Filter "ShareName='HB BIG TOSHIBA'"
 Invoke-CimMethod -InputObject $printer -MethodName SetDefaultPrinter
