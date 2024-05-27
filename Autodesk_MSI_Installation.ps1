fdf5522de6b96e24d87a46e09e6cba4344d214dd
Hardik-Thakkar <117873560+Hardik-Thakkar@users.noreply.github.com>
2024-05-27 03:34:39 -0700

    Update Autodesk_MSI_Installation.ps1

--- Autodesk_MSI_Installation.ps1
+++ Autodesk_MSI_Installation.ps1
@@ -1,4 +1,4 @@
-$Download = "https://rightclickinc.sharepoint.com/:u:/s/Software/ERPrwCORZKhFj2BoUZsd7aIBuIP_9_G6K1wtjeXn0hZeJQ?download=1"
+$Download = "https://contoso.sharepoint.com/:u:/s/Software/ERPrwCORZKhFj2BoUZsd7aIBuIP_9_G6K1wtjeXn0hZeJQ?download=1"
 Invoke-WebRequest -Uri $Download -OutFile C:\Utils\AutodeskDWGCompare2016-2019.msi -UseBasicParsing -ErrorAction Stop 
 
 If(-not(Test-Path C:\Util))
